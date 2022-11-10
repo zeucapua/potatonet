732c2bb8e6bfcc7850a98daccd364517c1a907c7
Zeu Capua <zeucapua@gmail.com>
2022-11-10 13:29:02 -0800

    change favicon, removed logo spin animation

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,5 @@
 <script lang="ts">
   import Icon from "@iconify/svelte";
-  import { Motion } from "svelte-motion";
-
   let tags = [
     "cats",
     "games",
@@ -11,23 +9,12 @@
     "travel",
   ];
 
-  let logoDeg : number = 0;
-
-
 </script>
 
 <div class="flex flex-col w-full min-w-screen h-full min-h-screen justify-center">
   
   <div class="flex flex-col bg-primary h-full min-h-screen justify-center">
-
-    <Motion animate={{ rotate: logoDeg }} transition={{ ease: 'easeOutIn', duration: .75 }} let:motion>
-      <!-- svelte-ignore a11y-click-events-have-key-events -->
-      <img src="potatonet-logo.png" class="w-1/3 h-1/3 mx-auto" alt="potatonet logo" 
-        use:motion on:click={ () => logoDeg += 360 }
-      />
-    </Motion>
-    
-
+    <img src="potatonet-logo.png" class="w-1/3 h-1/3 mx-auto" alt="potatonet logo" />
     <div class="flex flex-col gap-8 text-center mx-auto">
       <p class="text-8xl text-base-100 font-black">potatonet</p>
       <p class="text-3xl text-base-100 font-bold">There's more out there, Internet Explorer</p>
